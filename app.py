1589413dc3936fc368ad2dc6800222ff6c91cf54
Eldhose-Peter <eldhosepeter2000@gmail.com>
2022-06-05 22:40:14 +0530

    Changed port address

--- app.py
+++ app.py
@@ -10,6 +10,9 @@ from flask_cors import CORS
 app = Flask(__name__)
 CORS(app)
 
+if __name__ == '__main__':
+    app.run(host="localhost", port=3000)
+
 def get_data(url,token):
     response = requests.get(url,headers={'Authorization': token})
     data = response.json()
